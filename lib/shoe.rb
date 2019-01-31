e14ba8bff3cc9e08681f2387a419e67e697ac753
Mouni Mullapudi <mullapudi24@gmail.com>
2019-01-31 11:03:57 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,11 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand 
-  
-  def initialze(brand)
-    @brand = brand 
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
   end
-  
+
   def cobble
     @condition = "new"
     puts "Your shoe is as good as new!"
