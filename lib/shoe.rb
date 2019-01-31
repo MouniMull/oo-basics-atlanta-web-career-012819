b7b64f2d8e3aa6587e21731de3e154c95a1cfe09
Mouni Mullapudi <mullapudi24@gmail.com>
2019-01-31 11:01:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,11 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
+  attr_reader :brand 
+  
   def initialze(brand)
     @brand = brand 
   end
   
-  def brand 
-    @brand
-  end 
-  
+  def cobble 
+
   
